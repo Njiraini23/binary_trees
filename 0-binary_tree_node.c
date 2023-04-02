72198fe1950355f8c7f009d7819c30b20db18126
Njiraini23 <njiraini88@gmail.com>
2023-04-02 04:54:40 +0000

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,3 +1,25 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.>
 #include "binary_trees.h"
+
+/**
+ *binary_tree_node - Binary tree node
+ *
+ * @parent: Pointer to the parent node
+ * @value: the data of root node
+ * Return: binary_tree_t*
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+	node->parent = parent;
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
+}
 
